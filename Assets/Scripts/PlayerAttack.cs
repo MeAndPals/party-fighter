2779b039ef490e18844ff3d3489ce7d81bba3267
Rafiraf7642 <Rafiraf7642@gmail.com>
2018-11-20 22:50:20 +0600

    Umm added the stuff.
    IDK wat 2 say.

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+    public float checkRad;
+    public Transform checkPos;
+
+    public float damage;
+
+    public float TimetoAtk;
+    [SerializeField]
+    private float StartTime2Atk;
+    
+	// Use this for initialization
+	void Start () {
+        TimetoAtk = StartTime2Atk;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(TimetoAtk <= 0)
+        {
+
+            TimetoAtk = StartTime2Atk;
+        }
+        else
+        {
+            TimetoAtk -= Time.deltaTime;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    Rigidbody2D rb;
+    [SerializeField]
+    float JumpTime;
+    public float StartJumpTime;
+    public float JumpForce;
+    public float checkRad;
+    public Transform checkPos;
+    public LayerMask ground;
+    public bool IsGrounded;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        JumpTime = StartJumpTime;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        bool IsGrounded = Physics2D.OverlapCircle(checkPos.position, checkRad, ground);
+        float h = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(h * speed, rb.velocity.y);
+        if (JumpTime <= 0 && IsGrounded == true)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            }
+            JumpTime = StartJumpTime;
+        }
+        else
+        {
+            JumpTime -= Time.deltaTime; 
+        }
+	}
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(checkPos.position, checkRad);
+    }
+}
